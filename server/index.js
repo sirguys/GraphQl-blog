b3bb76b255a1009171a9acd2738485e5d80031c4
sirguys <sirguys.apple@gmail.com>
2018-12-19 02:42:35 +0700

    serve favicon

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
 const express = require('express')
+const path = require('path')
+const favicon = require('serve-favicon')
+
 const renderer = require('./renderer')
 
+const root = path.resolve(__dirname, '..')
+const publicPath = path.resolve(root, 'public')
+
 const app = express()
+
+app.use(favicon(path.resolve(publicPath, 'favicon.ico')))
+
+app.use(express.static(publicPath))
 
 app.use(renderer)
 
